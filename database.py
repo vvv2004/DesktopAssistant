10a112bfe924bd4c0b5567a43a79d2c5bea72a37
vvv2004 <48321407+vvv2004@users.noreply.github.com>
2023-07-11 13:08:14 +0300

    connection with firebase firestore database created save and get methods to save data and load data from database

---
+++ database.py
@@ -0,0 +1,22 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+class Database:
+    def __init__(self):
+        cred = credentials.Certificate("D:/Development/DesktopAssistant/secure/secure.json")
+        app = firebase_admin.initialize_app(cred)
+        self.db = firestore.client()
+
+    def save_to_database(self, dictionary, collection):
+        self.db.collection(collection).set(dictionary)
+
+    def get_from_database(self, collection, identifier):
+        reference = self.db.collection(collection).document(identifier)
+        doc = reference.get()
+
+        if doc.exists:
+            return doc.to_dict()
+        else:
+            return None
